ccfe94def932881481c559429c8a596285c8a459
MartinData01 <106952538+MartinData01@users.noreply.github.com>
2023-06-26 16:39:03 +0800

    change dropdown list style with jquery

--- js/all.js
+++ js/all.js
@@ -1,10 +1,28 @@
 $(document).ready(function(){
+    // 回到頂端
     $('#toTop').click(function(){
         $('html,body').animate({
             scrollTop: 0
         },800)
     });
+    // 手機版選單
     $('.navbar-btn').click(function(){
         $('.navbar-collapse').toggleClass('show');
     });
+    // 排序按鈕
+    $('.dropdown-btn').click(function(){
+        $('.dropdown-menu').toggleClass('show');
+    });
+    // 排序按鈕 - 改選單的字
+    $('.new-to-old').click(function(e) {
+        e.preventDefault();
+        $('.dropdown-menu').toggleClass('show');
+        $('.dropdown-btnText').text(this.text);
+      });
+      
+    $('.old-to-new').click(function(e) {
+        e.preventDefault();
+        $('.dropdown-menu').toggleClass('show');
+        $('.dropdown-btnText').text(this.text);
+      });
 });
